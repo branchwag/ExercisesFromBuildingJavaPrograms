302eda5b214e6c6fbc115d8dc08257c24f2971cf
LeAnne B. <branchleannelmb@gmail.com>
2025-02-12 20:46:45 -0500

    adds next few exercises done

---
+++ ch1p15.java
@@ -0,0 +1,5 @@
+class ch1p15 {
+	public static void main(String [] args) {
+		System.out.println("/ \\ // \\\\ /// \\\\\\");
+	}
+}
---
+++ ch1p16.java
@@ -0,0 +1,13 @@
+class ch1p16 {
+	public static void main(String [] args) {
+		System.out.println("System.out.print(\"Twas \");");
+		System.out.println("System.out.print(\"brillig and the\");");
+		System.out.println("System.out.printlm(\" \");");
+		System.out.println("System.out.print(\" slithy toves did\");");
+		System.out.println("System.out.print(\" \");");
+		System.out.println("System.out.println(\"gyre and\");");
+		System.out.println("System.out.println(\"gimble\");");
+		System.out.println("System.out.println();");
+		System.out.println("System.out.println(\"in the wabe. \");");
+	}
+}
