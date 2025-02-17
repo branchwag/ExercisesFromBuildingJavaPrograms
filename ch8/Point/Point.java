46d7038f79a9b3e97a0897722beac385c6d26d39
LeAnne B. <branchleannelmb@gmail.com>
2025-02-16 23:42:13 -0500

    allwos Point example to take empty args

--- ch8/Point/Point.java
+++ ch8/Point/Point.java
@@ -2,9 +2,14 @@ public class Point {
 	int x;
 	int y;
 
+	public Point() {
+		x = 0;
+		y = 0; 
+	}
+
 	public Point(int initialX, int initialY) {
-		x = initialX;
-		y = initialY;
+		this.x = initialX;
+		this.y = initialY;
 	}
 
 	public double distanceFromOrigin() {
--- ch8/Point/PointExample.java
+++ ch8/Point/PointExample.java
@@ -3,6 +3,8 @@ import java.awt.*;
 public class PointExample {
 	public static void main(String[] args) {
 		Point p1 = new Point(3, 8);
-		System.out.println("Distance from origin = " + p1.distanceFromOrigin());
+		Point p2 = new Point();
+		System.out.println("Distance from origin for p1 = " + p1.distanceFromOrigin());
+		System.out.println("Distance from origin for p2 = " + p2.distanceFromOrigin());
 	}
 }
