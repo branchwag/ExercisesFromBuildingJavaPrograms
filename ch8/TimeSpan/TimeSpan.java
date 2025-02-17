ea6de621610df90f874507a84492f354c8ea0df1
LeAnne B. <branchleannelmb@gmail.com>
2025-02-17 18:51:53 -0500

    ensuring previous version of Timespan class in ch8 is committed

---
+++ ch8/TimeSpan/TimeSpan.java
@@ -0,0 +1,28 @@
+public class TimeSpan {
+	private int hours; 
+	private int minutes;
+
+	public TimeSpan(int hours, int minutes) {
+		this.hours = 0;
+		this.minutes = 0;
+		add(hours, minutes);
+	}
+
+	public void add(int hours, int minutes) {
+		if (hours < 0 || minutes < 0) { 
+			throw new IllegalArgumentException();
+		}
+	
+		this.hours += hours;
+		this.minutes += minutes;
+
+		this.hours += this.minutes / 60;
+		this.minutes = this.minutes % 60; //what's left over are minutes
+	}
+
+	public String toString() {
+		return hours + "h " + minutes + "m";
+	}
+}
+
+
