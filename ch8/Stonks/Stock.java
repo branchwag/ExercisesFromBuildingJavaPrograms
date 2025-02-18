1990059a418af09130ab25b64ba6772a7060222b
LeAnne B. <branchleannelmb@gmail.com>
2025-02-17 21:33:01 -0500

    adds in work done so far on the Stock class

---
+++ ch8/Stonks/Stock.java
@@ -0,0 +1,33 @@
+public class Stock {
+	private String symbol;
+	private int totalShares;
+	private double totalCost;
+
+	public Stock(String theSymbol) {
+
+		if (theSymbol == null) {
+			throw new NullPointerException();
+		}
+
+		symbol = theSymbol;
+		totalShares = 0;
+		totalCost = 0.0
+	}
+
+	public double getProfit(double currentPrice) {
+		if (currentPrice < 0.0) {
+			throw new IllegalArgumentException();
+		}
+		//(current price * shares) - (cost * shares)
+		return totalShares * currentPrice - totalCost;
+	}
+
+	public void purchase(int shares, double pricePerShare) {
+		if (shares < 0 || pricePerShare < 0) {
+			throw new IllegalArgumentException();
+		}
+		//set totalCost equal to shares * pricePerShare
+		totalShares += shares;
+		totalCost += shares * pricePerShare;
+	}
+}
