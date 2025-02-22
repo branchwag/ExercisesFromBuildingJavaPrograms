c4b2b585bb227b8613c7b60060894ffb4749295f
LeAnne B. <branchleannelmb@gmail.com>
2025-02-22 17:54:24 -0500

    fixes up the dividendstock class as a subclass of stock class

--- ch8/Stonks/DividendStock.java
+++ ch8/Stonks/DividendStock.java
@@ -1,7 +1,19 @@
 public class DividendStock extends Stock {
 	private double dividends;
 
+	public DividendStock(String symbol) {
+		//call superclass constructor, pass it symbol
+		//get back init of symbol, totalShares, and totalCost
+		super(symbol);
+		this.dividends = 0.0;
+	}
+
 	public void payDividend(double amountPerShare) {
+		//dividends = dividends + (amountPerShare * getTotalShares());
 		dividends += amountPerShare * getTotalShares();	
+	}
+
+	public double getProfit(double currentPrice) {
+		return super.getProfit(currentPrice) + dividends;
 	}
 }
