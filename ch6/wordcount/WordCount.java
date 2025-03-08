89842ec56255c013ce410929ed69e0325c905c3b
LeAnne B. <branchleannelmb@gmail.com>
2025-03-08 14:33:40 -0500

    allows for file input retry on word count program

--- ch6/wordcount/WordCount.java
+++ ch6/wordcount/WordCount.java
@@ -3,12 +3,26 @@ import java.util.*;
 
 public class WordCount {
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner input = new Scanner(new File("hamlet.txt"));
+		Scanner console = new Scanner(System.in);
+		Scanner input = getInput(console);
 		int count = 0;
 		while (input.hasNext()) {
 			String word = input.next();
 			count++;
 		}
 		System.out.println("total words = " + count);
+	}
+
+
+	public static Scanner getInput(Scanner console) throws FileNotFoundException {
+		System.out.print("Input file: ");
+		File f = new File(console.nextLine());
+		while (!f.canRead()) {
+			System.out.println("File not found. Try again.");
+			System.out.print("Input file: ");
+			f = new File(console.nextLine());
+		}
+		//good at this point
+		return new Scanner(f);
 	}
 }
